003004b76f54b496da8d2a5d42b0f809410e28b6
phw4483 <gusdn3477@naver.com>
2021-01-20 13:39:42 +0900

    version 20210120

---
+++ members/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http.response import HttpResponse
+from .models import Members
+# Create your views here.
+
+def index(request):
+    return HttpResponse("<h1>hello</h1>")
+
+def test(request):
+    return HttpResponse("<h2>test</h2>")
+
+def signup(request):
+
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+
+        member = Members(
+                username = username,
+                useremail = email
+        )
+
+        member.save()
+        res_data = {}
+        res_data['res'] = '등록성공'
+
+        return render(request, 'index.html', res_data)
+
+    return render(request, 'index.html')
