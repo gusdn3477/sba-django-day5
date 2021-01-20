bc93655e9865a5f9c763895230b12219795e81ce
phw4483 <gusdn3477@naver.com>
2021-01-20 17:02:57 +0900

    로그인 추가

--- lotto/views.py
+++ lotto/views.py
@@ -4,9 +4,13 @@ import random
 
 # Create your views here.
 def index(request):
-    lotto = []
 
-    while len(lotto) < 6:
-        lotto.append(random.randint(1,46))
-        lotto = list(set(lotto))
-    return HttpResponse(f"<h1> 당신의 행운을 기원합니다</h1><br><h1>lotto 번호 추천 {lotto} </h1>")
+    if request.method == 'GET':
+        lotto = []
+
+        while len(lotto) < 6:
+            lotto.append(random.randint(1,46))
+            lotto = list(set(lotto))
+        return HttpResponse(f"<h1> 당신의 행운을 기원합니다</h1><br><h1>lotto 번호 추천 {lotto} </h1>")
+
+    return HttpResponse("post")
--- members/views.py
+++ members/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http.response import HttpResponse
 from .models import Members
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>hello</h1>")
+    #print(dir(request))
+    print(request.GET.get('id',''))
+    num = request.GET.get('id','')
+
+    if len(num) < 1:
+        return HttpResponse("<h1>version 1 : dynamic page</h1>")
+    
+    return HttpResponse(f"<h2> 구구단 : {num_gugu(num)}</h2>")
 
 def test(request):
     return HttpResponse("<h2>test</h2>")
@@ -43,3 +50,14 @@ def num_gugu(num):
         st += f"{num} * {i} = {int(num) * i}<br>"
      
     return st
+
+def login(request):
+
+    print(dir(request))
+
+    if request.method == 'GET':
+        return render(request, 'login.html')
+
+    elif request.method == 'POST':
+        return redirect('/') 
+
